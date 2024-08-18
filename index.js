7c3d59fd6d0e151fd01ef2ec0cb6de18d2dc6407
Abhiassa <abhiassaproject@gmail.com>
2024-08-18 14:23:38 +0700

    Update

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const cors = require('cors');
 const app = express();
 const port = 5000;
 
-app.use(cors, (req, res, next) => {
+app.use(cors());
+
+app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
