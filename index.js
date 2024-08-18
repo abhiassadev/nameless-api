a62945d60762665bdeb9051b974704d9bf88e6bf
abhiassadev <abhiassaproject@gmail.com>
2024-08-18 19:02:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 5000;
 
 app.use(cors());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
